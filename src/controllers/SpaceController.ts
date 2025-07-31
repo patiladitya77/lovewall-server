0312163d961d5be3677a30946f5690ac37b669f8
patiladitya77 <adpatil587@gmail.com>
2025-07-31 22:45:44 +0530

    createspace controller

--- src/controllers/SpaceController.ts
+++ src/controllers/SpaceController.ts
@@ -2,6 +2,7 @@ import { clerkClient, getAuth } from "@clerk/express";
 import { Request, Response } from "express";
 import User from "../models/User";
 import Space from "../models/Space";
+import { nanoid } from "nanoid";
 
 export const getSpacesController = async (req: Request, res: Response) => {
   try {
@@ -16,6 +17,9 @@ export const getSpacesController = async (req: Request, res: Response) => {
     const clerkUser = await clerkClient.users.getUser(auth.userId);
     const email = clerkUser?.emailAddresses[0]?.emailAddress;
     const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
     const spaces = await Space.find({ ownerId: user._id });
 
     res.json({
@@ -27,5 +31,46 @@ export const getSpacesController = async (req: Request, res: Response) => {
     res
       .status(500)
       .json({ error: "Internal Server Error", details: err.message });
+  }
+};
+
+export const createSpaceController = async (req: Request, res: Response) => {
+  try {
+    const auth = getAuth(req);
+    if (!auth.userId) {
+      return res.status(400).json({ message: "Unauthorised" });
+    }
+    const clerkUser = await clerkClient.users.getUser(auth.userId);
+    const {
+      spaceName,
+      spaceLogo,
+      headerTitle,
+      customMessage,
+      questions,
+      thankyouTitle,
+      thankyouMessage,
+    } = req.body;
+    const user = await User.findOne({
+      email: clerkUser?.emailAddresses[0]?.emailAddress,
+    });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const slug = nanoid(5);
+    const space = new Space({
+      spaceName: spaceName,
+      spaceLogo: spaceLogo,
+      headerTitle: headerTitle,
+      customMessage: customMessage,
+      questions: questions,
+      thankyouMessage: thankyouMessage,
+      thankyouTitle: thankyouTitle,
+      ownerId: user._id,
+      slug: slug,
+    });
+    const savedSpace = await space.save();
+    res.json({ message: "Space cretaed successfully", savedSpace });
+  } catch (err) {
+    res.status(400).json({ message: "ERROR " + err });
   }
 };
--- src/routes/SpaceRouter.ts
+++ src/routes/SpaceRouter.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { getSpacesController } from "../controllers/SpaceController";
+import {
+  createSpaceController,
+  getSpacesController,
+} from "../controllers/SpaceController";
 import { userAuth } from "../middlewares/userAuth";
 
 const SpaceRouter = express.Router();
 
 SpaceRouter.get("/getallspaces", userAuth, getSpacesController);
+SpaceRouter.post("/createspace", userAuth, createSpaceController);
 export default SpaceRouter;
