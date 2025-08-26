504aa05873833755be4994a375287e648d9c2e26
patiladitya77 <adpatil587@gmail.com>
2025-08-26 21:43:40 +0530

    update:testimonial schema

--- src/models/Testimonial.ts
+++ src/models/Testimonial.ts
@@ -1,41 +1,46 @@
 import mongoose from "mongoose";
 
-const testimonialSchema = new mongoose.Schema({
-  spaceId: {
-    type: mongoose.Schema.Types.ObjectId,
-    requierd: true,
-    ref: "Space",
+const testimonialSchema = new mongoose.Schema(
+  {
+    spaceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      requierd: true,
+      ref: "Space",
+    },
+    starRating: {
+      type: Number,
+      required: true,
+    },
+    feedback: {
+      type: String,
+    },
+    feedbackType: {
+      type: String,
+      enum: ["text", "video", "textandvideo"],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    senderEmail: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+    },
+    isArchived: {
+      type: Boolean,
+    },
+    isLiked: {
+      type: Boolean,
+    },
   },
-  starRating: {
-    type: Number,
-    required: true,
-  },
-  feedback: {
-    type: String,
-  },
-  feedbackType: {
-    type: String,
-    enum: ["text", "video", "textandvideo"],
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  senderEmail: {
-    type: String,
-    required: true,
-  },
-  videoUrl: {
-    type: String,
-  },
-  isArchived: {
-    type: Boolean,
-  },
-  isLiked: {
-    type: Boolean,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Testimonial = mongoose.model("Testimonial", testimonialSchema);
 
