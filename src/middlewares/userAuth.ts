9ccc6d7a721ec9a1c255e636faaf3fe22df711b9
patiladitya77 <adpatil587@gmail.com>
2025-12-08 18:40:10 +0530

    refactor: userAuth middleware

--- src/middlewares/userAuth.ts
+++ src/middlewares/userAuth.ts
@@ -1,5 +1,6 @@
 import { getAuth, clerkClient } from "@clerk/express";
 import { Request, Response, NextFunction } from "express";
+import User from "../models/User";
 
 export const userAuth = async (
   req: Request,
@@ -13,8 +14,13 @@ export const userAuth = async (
     }
 
     const user = await clerkClient.users.getUser(auth.userId);
+    const email = user?.emailAddresses?.[0]?.emailAddress;
+    if (!email) {
+      return res.status(400).json({ error: "User email not found" });
+    }
+    const localUser = await User.findOne({ email });
 
-    (req as any).user = user;
+    (req as any).user = localUser;
 
     next();
   } catch (err) {
